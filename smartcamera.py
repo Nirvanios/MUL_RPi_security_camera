e367f005dcbdf2421d0cd7b8de94c18d80769555
Petr <xflajs00@stud.fit.vutbr.cz>
2019-04-26 14:17:09 +0200

    populate config with valid values, fix type errors

--- smartcamera.py
+++ smartcamera.py
@@ -37,7 +37,7 @@ def combine_boxes(boxes, config):
             for box2 in boxes:
                 if box == box2:
                     continue
-                if box.distance(box2) < config.get_value("bb_max_distance"):
+                if box.distance(box2) < config.get_value("cv.bb_max_distance"):
                     boxes[boxes.index(box)] = box.combine(box2)
                     boxes.remove(box2)
                     change = True
@@ -64,21 +64,26 @@ def detect_and_notify(config: mulconfig.Config):
     camera = npcamera.Camera((config.get_value("cv.resolution.width"), config.get_value("cv.resolution.height")))
     locator = tracking.ChangeLocator(camera.capture_image())
     tracked = None
+    cnt = 0
     while True:
         img = camera.capture_image()
-        person_bb = find_person(locator.detect_change(img))
+        person_bb = find_person(locator.detect_change(img), config)
         if person_bb is not None:
             if tracked is None:
                 tracked = tracking.TrackedObject(datetime.datetime.now())
-                saving_queue.put((QueueItem.file_start, "filename.mp4"))
+                saving_queue.put((QueueItem.file_start, str(cnt) + ".mp4"))
+                l.log(LogLevel.DEBUG, "Creating tracker")
+                cnt += 1
             else:
                 tracked.reset_lost()
+                l.log(LogLevel.DEBUG, "Resetting tracker")
             saving_queue.put((QueueItem.image, (img, person_bb)))
         else:
             if tracked is not None:
                 tracked.inc_lost()
                 saving_queue.put((QueueItem.image, (img, None)))
-                if tracked.get_lost_cnt() > config.get_value("lost_frames_max"):
+                if tracked.get_lost_cnt() > config.get_value("cv.lost_frames_max"):
+                    l.log(LogLevel.DEBUG, "Lost object")
                     tracked.mark_lost(datetime.datetime.now())
                     saving_queue.put((QueueItem.file_end, (tracked.get_times())))
                     tracked = None
@@ -91,35 +96,41 @@ def acquire_and_save(config: mulconfig.Config, on_saving_done):
     """
     state = SaveThreadState.waiting
     writer = None
+    frame_cnt = 0
     resolution = config.get_value("cv.resolution.width"), config.get_value("cv.resolution.height")
+    bbs = []
     while True:
         (item_type, item) = saving_queue.get()
-        if state == SaveThreadState.waiting and item_type is QueueItem.file_start:
-            writer = videoutils.VideoWriter(config.get_value("save_location") + item, resolution, 12.0)
-            state = SaveThreadState.saving_imgs
-            l.log(LogLevel.DEBUG, "Start saving")
-        else:
-            l.log(LogLevel.ERROR,
-                  "Invalid item type in saving thread. State: " + state.name + " item type: " + item_type.name)
+        if state == SaveThreadState.waiting:
+            if item_type is QueueItem.file_start:
+                writer = videoutils.VideoWriter(config.get_value("save_location") + item, resolution, 12.0)
+                state = SaveThreadState.saving_imgs
+                l.log(LogLevel.DEBUG, "Start saving")
+            else:
+                l.log(LogLevel.ERROR,
+                      "Invalid item type in saving thread. State: " + state.name + " item type: " + item_type.name)
         if state == SaveThreadState.saving_imgs:
             if item_type is QueueItem.image:
                 img = item[0]
-                if item[2] is not None:
-                    (x1, y1, x2, y2) = item[2]
-                    cv2.rectangle((x1, y1), (x2, y2), (0, 255, 0), 2)
+                if item[1] is not None:
+                    bbs.append((item[1], frame_cnt))
+                    bb = item[1]
+                    cv2.rectangle(img, (bb.p1[0], bb.p1[1]), (bb.p2[0], bb.p2[1]), (0, 255, 0), 2)
                 writer.write_frame(img)
-                l.log(LogLevel.DEBUG, "Writing frame")
+                frame_cnt += 1
             elif item_type is QueueItem.file_end:
                 writer.close()
                 state = SaveThreadState.waiting
-                on_saving_done(writer.dest_path, item)
+                on_saving_done(writer.dest_path, item[0], item[1], bbs)
                 l.log(LogLevel.DEBUG, "Saving finished to:" + writer.dest_path)
+                frame_cnt = 0
+                bbs.clear()
             else:
                 l.log(LogLevel.ERROR,
                       "Invalid item type in saving thread. State: " + state.name + " item type: " + item_type.name)
 
 
-def on_saving_done(file_path: str, start_time, end_time):
+def on_saving_done(file_path: str, start_time, end_time, bbs):
     l.log(LogLevel.DEBUG, "Saving done, notifying...")
 
 
--- tracking.py
+++ tracking.py
@@ -54,7 +54,6 @@ class ChangeLocator:
 
         thresh = cv2.threshold(frame_delta, self.thresh, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
-        cv2.imshow("t", thresh)
 
         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = imutils.grab_contours(contours)
